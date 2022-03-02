c584e7cd576325577877716687f66c4265d4dbdf
= <=>
2022-03-02 18:08:13 -0500

    added all disabled users aren't allowed to login at any time

--- secureADStart.ps1
+++ secureADStart.ps1
@@ -10,8 +10,16 @@ Add-ADGroupMember -Identity Administrators -Members $userName
 Add-ADGroupMember -Identity "Domain Admins" -Members $userName
 Add-ADGroupMember -Identity "Schema Admins" -Members $userName
 
+
+
+$Hours = New-Object byte[] 21
+$replaceHashTable = New-Object HashTable
+$replaceHashTable.Add("logonHours", $Hours)
 foreach ($user in enabledUsers) {
+    Set-AdUser -Identity $user -Replace $replaceHashTable
     Disable-ADAccount -Identity $user
-    remove-adgroupmember -identity Administrators -members $user -verbose -confirm:$false
+    if ($user -ne "Administrator") {
+        remove-adgroupmember -identity Administrators -members $user -verbose -confirm:$false
+    }
 }
 
